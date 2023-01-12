7907260ab67338eca1e9178fc57985856a7a11ad
mohamedmahmoud6196 <33757410+mohamedmahmoud6196@users.noreply.github.com>
2023-01-12 17:23:08 +0200

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do anaaaay additional setup after loading the view.
     print("Hello sssx ")
+        let x = 8
     }
 
 
